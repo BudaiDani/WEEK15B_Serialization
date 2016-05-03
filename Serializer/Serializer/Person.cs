d3ed2e5d26f11fd8ba279a4af52ea2848af3c8b8
Budai Dani <budai.d945@gmail.com>
2016-05-03 13:53:55 +0200

    form ready person class getters,setters done

---
+++ Serializer/Serializer/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer
+{
+    [Serializable]
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private int phone;
+
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        private DateTime dateOfRec;
+
+        public DateTime DateofRec
+        {
+            get { return dateOfRec; }
+            set { dateOfRec = value; }
+        }
+
+        private int serial;
+
+        public int Serial
+        {
+            get { return serial; }
+            set { serial = value; }
+        }
+    }
+}
